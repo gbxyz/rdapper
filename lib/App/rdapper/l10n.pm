ce4c181beb78d25bc4de6cd5dea358aefd28a6a9
Gavin Brown <gavin.brown@fastmail.uk>
2025-07-21 14:10:37 +0100

    added

---
+++ lib/App/rdapper/l10n.pm
@@ -0,0 +1,14 @@
+package App::rdapper::l10n;
+use base qw(Locale::Maketext::Gettext);
+
+1;
+
+package App::rdapper::l10n::en;
+use base qw(Locale::Maketext::Gettext);
+
+1;
+
+package App::rdapper::l10n::fr;
+use base qw(Locale::Maketext::Gettext);
+
+1;
